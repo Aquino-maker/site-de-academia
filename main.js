6bba83349425811b8a2990890b9dd2f34e6f96e6
Luan Aquino <luan.aq.souza2004@gmail.com>
2025-02-25 17:21:56 -0300

    Terminando o CSS, adicionando DATA E HORA ao site

--- main.js
+++ main.js
@@ -6,3 +6,29 @@ menu.onclick = () =>{
     navbar.classList.toggle('active');
 };
 
+window.onscroll = () =>{
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+};
+
+function dateHour() {
+    const now = new Date();
+
+    const day = now.getDate().toString().padStart(2, '0');
+    const month = (now.getMonth() + 1).toString().padStart(2, '0');
+    const year = now.getFullYear();
+
+    const hour = now.getHours().toString().padStart(2, '0');
+    const minute = now.getMinutes().toString().padStart(2, '0');
+    const seconds = now.getSeconds().toString().padStart(2, '0');
+
+    const dateCurrent = `${day}/${month}/${year}`;
+    const hourCurrent = `${hour}:${minute}:${seconds}`;
+
+    const dateHourCurrent = `${dateCurrent} ${hourCurrent}`;
+
+    document.getElementById("date_hour").textContent = dateHourCurrent;
+}
+
+dateHour();
+setInterval(dateHour, 1000)
